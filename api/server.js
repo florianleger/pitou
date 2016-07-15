109e9c8ea7668f4ab32dc95adffdd2aec3b7258b
Florian Leger <contact@florianleger.fr>
2016-07-15 12:13:50 +0200

    Add routes for getting frames.

--- api/server.js
+++ api/server.js
@@ -6,56 +6,78 @@ var app = express();
 var db;
 var _lastFrame = {};
 
-MongoClient.connect('mongodb://localhost:27017/pitou', function(err, database) {
-  if (err) {
-    throw err;
-  }
-  db = database;
+MongoClient.connect('mongodb://localhost:27017/pitou', function (err, database) {
+    if (err) {
+        throw err;
+    }
+    db = database;
 });
 
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-  res.send('Welcome to Pitou API ;)\n\n'
-   	+'Instant consumption: ' + _lastFrame.instantConsumption + ' W');
+    res.send(
+        '<h2>Pitou API</h2><br><br>' +
+        '<ul><li>Instant consumption: <strong>' + _lastFrame.instantConsumption + ' W</strong></li>' +
+        '<li>Peak hours index: <strong>' + _lastFrame.peakHours + ' Wh</strong></li>' +
+        '<li>Off-peak hours index: <strong>' + _lastFrame.offPeakHours + ' Wh</strong></li>' +
+        '<li>Current mode: <strong>' + _lastFrame.mode + '</strong></li></ul>'
+    );
 });
 
-app.post('/frames', function(req, res) {
-  if(req.body && req.body.frames) {
-    var frames = req.body.frames;
+app.get('/frames/last', function (req, res) {
+    res.json(_lastFrame);
+});
 
-    // Store frames
-    db.collection('frames').insert(frames, function(err, result) {
-      if (err) {
-        throw err;
-      }
-      console.log("New stored frame(s).");
+app.get('/frames', function (req, res) {
+    db.collection('frames').find({}).toArray(function(err, result) {
+        if(err) {
+            res.sendStatus(500);
+            throw err;
+        } else {
+            res.json(result);
+        }
     });
+});
 
-    // Keep the last frame in memory
-    _lastFrame = frames[frames.length - 1];
-  }
-  res.sendStatus(200);
+app.post('/frames', function (req, res) {
+    if (req.body && req.body.frames) {
+        var frames = req.body.frames;
+
+        // Store frames
+        db.collection('frames').insert(frames, function (err, result) {
+            if (err) {
+                res.sendStatus(500);
+                throw err;
+            } else {
+                res.sendStatus(200);
+                console.log("New stored frame(s).");
+            }
+        });
+
+        // Keep the last frame in memory
+        _lastFrame = frames[frames.length - 1];
+    }
 });
 
 /*app.post('/delete', function(req, res) {
-  _removeAllFrames(function(err) {
-    if(err) {
-      res.sendStatus(500);
-    }
-    res.sendStatus(200);
-  });
-});
+ _removeAllFrames(function(err) {
+ if(err) {
+ res.sendStatus(500);
+ }
+ res.sendStatus(200);
+ });
+ });
 
-var _removeAllFrames = function(callback) {
-   db.collection('frames').deleteMany(
-      { },
-      function(err, results) {
-         console.log(results);
-         callback();
-      }
-   );
-};
-*/
+ var _removeAllFrames = function(callback) {
+ db.collection('frames').deleteMany(
+ { },
+ function(err, results) {
+ console.log(results);
+ callback();
+ }
+ );
+ };
+ */
 
 app.listen(9841);
