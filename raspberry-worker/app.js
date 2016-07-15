2b7637aa9282e8feb1769e0238b171529673ec05
Florian Leger <contact@florianleger.fr>
2016-07-15 11:39:18 +0200

    Reformat teleinfo frames.

--- raspberry-worker/app.js
+++ raspberry-worker/app.js
@@ -2,6 +2,7 @@ var util = require('util');
 var teleinfo = require('./teleinfo.js');
 var request = require('request');
 
+var _unsentFrames = [];
 var trameEvents = teleinfo.teleinfo('/dev/ttyAMA0');
 
 // Handle elec information
@@ -10,14 +11,28 @@ trameEvents.on('onDecodedFrame', function (data) {
 });
 
 /**
- * Sends the buffered data to the API and empty the buffer
+ * Sends a frame to the API with buffered ones.
+ * @param {Object} data - The frame to send.
  */
 function sendData(data) {
+    var frames = [{
+        peakHours: data.HCHP,
+        offPeakHours: data.HCHC,
+        instantConsumption: data.PAPP,
+        mode: data.HHPHC,
+        date: Date.now()
+    }];
+
+    if(_unsentFrames.length > 0) {
+        frames = frames.concat(_unsentFrames);
+        _unsentFrames = [];
+    }
+
     var options = {
         uri: 'http://theyetifield.tk:9841/frames/',
         method: 'POST',
         json: {
-            frames: [data]
+            frames: frames
         }
     };
 
@@ -25,7 +40,9 @@ function sendData(data) {
         if (!error && response.statusCode == 200) {
             console.log("Frames successfully sent.");
         } else {
-            console.error('An error occurred when sending frames:', response.statusCode, 'error: ', error);
+            console.error('An error occurred when sending frames:', response.statusCode);
+            console.log('Retry to send them on the next request.');
+            _unsentFrames = frames;
         }
     });
 }
